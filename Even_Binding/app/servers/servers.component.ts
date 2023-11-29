633fb462384e11045963cc1845d2ccbe43aa309f
Pragati Priya <97381739+prag88@users.noreply.github.com>
2023-11-29 09:47:14 +0530

    Add files via upload
    Tried How even binding actually works!

---
+++ Even_Binding/app/servers/servers.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "no server was created"
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  
+  }
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created!";
+  }
+}
